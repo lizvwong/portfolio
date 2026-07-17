1e5e81ea80bd2c6bbefa00f07efecf3d008831cf
kunomori <kunomoriii@gmail.com>
2026-07-17 01:04:37 -0700

    batch 1: add html pages

--- script.js
+++ script.js
@@ -26,3 +26,27 @@ carousels.forEach(carousel => {
     updateCarousel();
   });
 });   
+
+const carousels_big = document.querySelectorAll('.carousel_big');
+carousels_big.forEach(carousel => {
+  const inner = carousel.querySelector('.carousel-inner');
+  const slides = carousel.querySelectorAll('.slide');
+  const prevBtn = carousel.querySelector('.prev');
+  const nextBtn = carousel.querySelector('.next');
+  
+  let index = 0; 
+
+  function updateCarousel() {
+    inner.style.transform = `translateX(-${index * 100}%)`;
+  }
+
+  nextBtn.addEventListener('click', () => {
+    index = (index + 1) % slides.length;
+    updateCarousel();
+  });
+
+  prevBtn.addEventListener('click', () => {
+    index = (index - 1 + slides.length) % slides.length;
+    updateCarousel();
+  });
+});   
